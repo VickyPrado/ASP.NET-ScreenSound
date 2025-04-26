4200019c18c83c093080cf8a17deb4fa835811f1
VickyPrado <vicky.prado05@gmail.com>
2025-04-26 18:51:39 -0300

    atualizando-add-artistas-com-context-Add-SaveChanges

--- ScreenSound/Banco/ArtistaDAL.cs
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -8,6 +8,7 @@ namespace ScreenSound.Banco;
 internal class ArtistaDAL
 {
     private readonly ScreenSoundContext context;
+
     public ArtistaDAL()
     {
         context = new ScreenSoundContext();
@@ -16,55 +17,44 @@ internal class ArtistaDAL
     {  
         return context.Artistas.ToList();
     }
+    
+    public void Adicionar(Artista artista)
+    {
+        context.Artistas.Add(artista);
+        context.SaveChanges();
+    }
 
     /*
-  public void Adicionar(Artista artista)
-  {
-      using var connection = new ScreenSoundContext().ObterConexao();
-      connection.Open();
+    public void Atualizar(Artista artista)
+    {
+        using var connection = new ScreenSoundContext().ObterConexao();
+        connection.Open();
 
-      string sql = "INSERT INTO Artistas (Nome, FotoPerfil, Bio) VALUES (@nome, @perfilPadrao, @bio)";
-      SqlCommand command = new SqlCommand(sql, connection);
+        string sql = "UPDATE Artistas SET Nome = @nome, FotoPerfil = @perfilPadrao, Bio = @bio WHERE Id = @id";
+        SqlCommand command = new SqlCommand(sql, connection);
 
-      command.Parameters.AddWithValue("@nome", artista.Nome);
-      command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
-      command.Parameters.AddWithValue("@bio", artista.Bio);
+        command.Parameters.AddWithValue("@nome", artista.Nome);
+        command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
+        command.Parameters.AddWithValue("@bio", artista.Bio);
+        command.Parameters.AddWithValue("@id", artista.Id);
 
-      int retorno = command.ExecuteNonQuery();
-      Console.WriteLine($"Linhas afetadas: {retorno}");
-  }
-
-  
-  public void Atualizar(Artista artista)
-  {
-      using var connection = new ScreenSoundContext().ObterConexao();
-      connection.Open();
-
-      string sql = "UPDATE Artistas SET Nome = @nome, FotoPerfil = @perfilPadrao, Bio = @bio WHERE Id = @id";
-      SqlCommand command = new SqlCommand(sql, connection);
-
-      command.Parameters.AddWithValue("@nome", artista.Nome);
-      command.Parameters.AddWithValue("@perfilPadrao", artista.FotoPerfil);
-      command.Parameters.AddWithValue("@bio", artista.Bio);
-      command.Parameters.AddWithValue("@id", artista.Id);
-
-      int retorno = command.ExecuteNonQuery();
-      Console.WriteLine($"Linhas afetadas: {retorno}");
-  }
+        int retorno = command.ExecuteNonQuery();
+        Console.WriteLine($"Linhas afetadas: {retorno}");
+    }
 
 
-  public void Deletar(int id)
-  {
-      using var connection = new ScreenSoundContext().ObterConexao();
-      connection.Open();
+    public void Deletar(int id)
+    {
+        using var connection = new ScreenSoundContext().ObterConexao();
+        connection.Open();
 
-      string sql = "DELETE FROM Artistas WHERE Id = @id";
-      SqlCommand command = new SqlCommand(sql, connection);
+        string sql = "DELETE FROM Artistas WHERE Id = @id";
+        SqlCommand command = new SqlCommand(sql, connection);
 
-      command.Parameters.AddWithValue("@id", id);
+        command.Parameters.AddWithValue("@id", id);
 
-      int retorno = command.ExecuteNonQuery();
-      Console.WriteLine($"Linhas afetadas: {retorno}");
-  }
-  */
+        int retorno = command.ExecuteNonQuery();
+        Console.WriteLine($"Linhas afetadas: {retorno}");
+    }
+    */
 }
