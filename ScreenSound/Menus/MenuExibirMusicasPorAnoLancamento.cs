52348f5e8a9d42e359b9f03c1362342e54f28f91
VickyPrado <vicky.prado05@gmail.com>
2025-05-03 18:33:54 -0300

    menu-listar-musicas-por-anoLancamento

---
+++ ScreenSound/Menus/MenuExibirMusicasPorAnoLancamento.cs
@@ -0,0 +1,42 @@
+﻿using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Menus
+{
+    internal class MenuMostrarMusicasPorAnoLancamento : Menu
+    {
+        public override void Executar(DAL<Artista> artistaDAL)
+        {
+            base.Executar(artistaDAL);
+            ExibirTituloDaOpcao("Mostrar músicas por ano de lançamento");
+            Console.Write("Digite o ano para consultar as músicas: ");
+            string anoLancamento = Console.ReadLine()!;
+            var musicaDAL = new DAL<Musica>(new ScreenSoundContext());
+            var listarAnoLancamento = musicaDAL.ListarPor(a => a.AnoLancamento == int.Parse(anoLancamento));
+
+            if (listarAnoLancamento.Any())
+            {
+                Console.WriteLine($"\nMusicas do Ano {anoLancamento}:");
+                foreach(var musica in listarAnoLancamento)
+                {
+                    musica.ExibirFichaTecnica();
+                }
+                Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            else
+            {
+                Console.WriteLine($"\nNenhuma música foi encontrada para o ano {anoLancamento}");
+                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
